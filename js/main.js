d0a84b7328e9c1023ea58dd1ba719cb9abb0e0ab
kelly <kelly.n.waite@gmail.com>
2016-10-11 15:33:45 -0600

    feat(core) Add culculate(), and functions for +, -, /, *, %
    Tested and work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 "use strict";
 
 console.log('cheese and puppies');
+
+function add (x, y) {
+  // console.log(x + y);
+  return x + y;
+}
+
+function subtract (x, y) {
+  // console.log(x - y);
+  return x - y;
+}
+
+function multiply (x, y) {
+  // console.log(x * y);
+  return x * y;
+}
+
+function divide (x, y) {
+  // console.log(x / y);
+  return x / y;
+}
+
+function remainder (x, y) {
+  return x % y;
+}
+
+function calculate (x, y, cb) {
+  console.log(cb(x, y));
+  return cb(x, y);
+}
